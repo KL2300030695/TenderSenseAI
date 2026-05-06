5fb1a806e7528522fe1f0041956eef03edbe8660
Vadaparthi Subhash <vadaparthysubhash@gmail.com>
2026-05-06 07:00:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3862366934-8e088",
+  "appId": "1:215136948173:web:42aa7794d35f6d92b97e84",
+  "apiKey": "AIzaSyBEhZQP5B8I37HQLxptsn35_lGRBXtBoe8",
+  "authDomain": "studio-3862366934-8e088.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "215136948173"
+};
